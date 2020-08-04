fde5b375bdd9688fecdcafb2b763d5539654efde
Niek Candaele <22315101+niekcandaele@users.noreply.github.com>
2020-08-04 13:35:00 +0200

    Add handle and starting side to team entity (#42)

--- entities/team.entity.ts
+++ entities/team.entity.ts
@@ -9,6 +9,7 @@ import {
   PrimaryGeneratedColumn
 } from 'typeorm';
 
+import { TeamType } from '../types/TeamType.enum';
 import { Match } from './match.entity';
 import { Player } from './player.entity';
 import { Round } from './round.entity';
@@ -29,6 +30,20 @@ export class Team extends BaseEntity {
 
   @Column({ nullable: true })
   name: string;
+
+  /**
+   * Uniquely identifies this team inside a demo
+   * Used for internal parsing of demo
+   * Not useful outside.. I think?
+   */
+  @Column()
+  handle: number;
+
+  @Column({
+    type: "enum",
+    enum: Object.values(TeamType)
+  })
+  startingSide: TeamType;
 
   @ManyToMany(() => Player, (player) => player.teams)
   @JoinTable()
---
+++ types/TeamType.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * @see https://saul.github.io/demofile/modules/_entities_team_.html#teamname
+ */
+export enum TeamType {
+  Unassigned = 0,
+  Spectator,
+  Terrorist,
+  CounterTerrorist,
+}
